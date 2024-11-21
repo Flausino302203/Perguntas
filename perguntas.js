eb7fd48dd276c55005167236dfdc55a41b560244
Flausino302203 <00001099941325sp@al.educacao.sp.gov.br>
2024-11-21 13:42:05 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,23 @@
 criaCartao(
-    'Preguntas Bíblicas',
-    'Qual o capítulo mais conhecido da bíblia?',
+    'Perguntas Bíblicas',
+    'Qual o capítulo mais conhecido da Bíblia?',
     'Salmos 23'
-)
+);
 
 criaCartao(
-    'Preguntas Bíblicas',
-    'Onde Jeusus nasceu?',
+    'Perguntas Bíblicas',
+    'Onde Jesus nasceu?',
     'Belém'
-)
+);
 
 criaCartao(
-    'Preguntas Bíblicas',
-    ' Quem foi lançado na cova dos leões?',
+    'Perguntas Bíblicas',
+    'Quem foi lançado na cova dos leões?',
     'Daniel'
-)
+);
 
 criaCartao(
-    'Preguntas Bíblicas',
-    ' Qual o nome do lugar onde habitavam Adão e Eva?',
-    'Jardim do Eden'
-)
-
+    'Perguntas Bíblicas',
+    'Qual o nome do lugar onde habitavam Adão e Eva?',
+    'Jardim do Éden'
+);
