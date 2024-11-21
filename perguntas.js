403de56fc0616e78e8040fdc80887ad853209168
Flausino302203 <00001099941325sp@al.educacao.sp.gov.br>
2024-11-21 14:11:52 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -2,20 +2,23 @@ criaCartao(
     'Perguntas Bíblicas',
     'Qual o nome do lugar onde habitavam Adão e Eva?',
     'Jardim do Éden'
+)
 
 criaCartao(
     'Perguntas Bíblicas',
     'Qual o nome do lugar onde habitavam Adão e Eva?',
     'Jardim do Éden'    
+)
 
 criaCartao(
     'Perguntas Bíblicas',
     'Qual o nome do lugar onde habitavam Adão e Eva?',
     'Jardim do Éden'
+)
 
 criaCartao(
     'Perguntas Bíblicas',
     'Qual o nome do lugar onde habitavam Adão e Eva?',
     'Jardim do Éden'    
-
+)
     
