93a7cb724e8e54a4a12854571f5e7ae89e38dc94
Flausino302203 <00001099941325sp@al.educacao.sp.gov.br>
2024-11-23 12:51:30 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -6,13 +6,13 @@ criaCartao(
 
 criaCartao(
     'Perguntas Bíblicas',
-    ' Quem foi lançado na cova dos leões?',
+    'Quem foi lançado na cova dos leões?',
     'Daniel'    
 )
 
 criaCartao(
     'Perguntas Bíblicas',
-    'Onde jesus nasceu?',
+    'Onde Jesus nasceu?',
     'Belém'
 )
 
@@ -32,4 +32,58 @@ criaCartao(
     'Perguntas Bíblicas',
     'Qual o primeiro milagre de Jesus?',
     'Transformar água em vinho'
+)
+
+criaCartao(
+    'Perguntas Bíblicas',
+    'Qual Apóstolo era irmão de Tiago?',
+    'João'
+)
+
+criaCartao(
+    'Perguntas Bíblicas',
+    'Qual cidade teve sua muralha derrubada?',
+    'Jericó'
+)
+
+criaCartao(
+    'Perguntas Bíblicas',
+    'Quem subiu aos céus em uma carruagem de fogo?',
+    'Elias'
+)
+
+criaCartao(
+    'Perguntas Bíblicas',
+    'Quem derrotou Golias?',
+    'Davi'
+)
+
+criaCartao(
+    'Perguntas Bíblicas',
+    'Quem foi colocado num cesto e lançado no rio ainda quando era bebê?',
+    'Moisés'
+)
+
+criaCartao(
+    'Perguntas Bíblicas',
+    'Quem entrou na terra prometida?',
+    'Josué'
+)
+
+criaCartao(
+    'Perguntas Bíblicas',
+    'Quem escreveu Salmos 23?',
+    'Davi'
+)
+
+criaCartao(
+    'Perguntas Bíblicas',
+    'Qual profeta foi engolido por um grande peixe?',
+    'Jonas'
+)
+
+criaCartao(
+    'Perguntas Bíblicas',
+    'Quantos pães e peixes Jesus utilizou para multiplicar e alimentar a multidão?',
+    '5 Pães e 2 Peixes'
 )
