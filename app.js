2fb6b271fbe9cc8f3c8addf08912c757ad28458c
Flausino302203 <00001099941325sp@al.educacao.sp.gov.br>
2024-11-23 12:03:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('container');
-    let cartao = document.createElement('article');
-    cartao.className = 'cartao';
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
     cartao.innerHTML = `
     <div class="cartao__conteudo">
-        <h3>${categoria}</h3>
+        <h3 class="cartao__titulo">${categoria}</h3>
         <div class="cartao__conteudo__pergunta">
             <p>${pergunta}</p>
         </div>
@@ -13,19 +13,23 @@ function criaCartao(categoria, pergunta, resposta) {
             <p>${resposta}</p>
         </div>
     </div>
-    `;
+    `
 
-    let respostaEstaVisivel = false;
+    let respostaEstaVisivel = false
 
-    // Função para virar o cartão
     function viraCartao() {
-        respostaEstaVisivel = !respostaEstaVisivel;
-        cartao.classList.toggle('active', respostaEstaVisivel); // Controla a classe 'active' para girar o cartão
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+
+        const titulo = cartao.querySelector('.cartao__titulo')
+        if (respostaEstaVisivel) {
+            titulo.style.display = 'none'
+        } else {
+            titulo.style.display = 'block'
+        }
     }
+    
+    cartao.addEventListener('click', viraCartao)
 
-    // Adiciona o evento de clique para virar o cartão
-    cartao.addEventListener('click', viraCartao);
-
-    // Adiciona o cartão ao container
-    container.appendChild(cartao);
+    container.appendChild(cartao)
 }
